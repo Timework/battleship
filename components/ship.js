b249b7e931676f61f894bf924bb7fbbcd365534d
Christopher Martinez <timework.bibo@gmail.com>
2021-01-05 06:15:31 -0600

    added the area function to the ship

--- components/ship.js
+++ components/ship.js
@@ -1,5 +1,31 @@
-const ship = (size) => {
-    return {size}
-}
+const Ship = (size) => {
+    // this will hold the area of the ship
+    let area = [];
 
-module.exports.ship = ship;
+    // this will run when object is made
+    const initalize = () => {
+        makeArea();
+    };
+
+    // making the area of the ship
+    const makeArea = () => {
+        for (let i = 0; i < size; i++) {
+            area.push(0);
+        };
+    };
+
+    // hitting a spot function
+    const hit = (spot) => {
+        if (area[spot] === 0) {
+        area[spot] = 1
+        } else {
+            return "This spot was already hit"
+        };
+    };
+
+    // runs initalize
+    initalize();
+    return {size, area, hit};
+};
+
+module.exports.Ship = Ship;
--- index.test.js
+++ index.test.js
@@ -1,13 +1,35 @@
 const rewire = require('rewire');
 
 // tests for the ship constructor
-const ship = rewire('./components/ship').__get__("ship");
+const Ship = rewire('./components/ship').__get__("Ship");
 
 test('ship length is correct', () => {
-    expect(ship(4).size).toBe(4);
+    expect(Ship(4).size).toBe(4);
 });
 
 test('checking ship length with a different number', () => {
-    expect(ship(2).size).toBe(2);
+    expect(Ship(2).size).toBe(2);
+});
+
+test('checking ship makes all the spots', () => {
+    expect(Ship(2).area).toMatchObject([0, 0]);
+});
+
+test('checking ship makes all the spots with a different size', () => {
+    expect(Ship(3).area).toMatchObject([0, 0, 0]);
+});
+
+// a function to help testing the hit function
+const testingHit = (test, number) => {
+    test.hit(number);
+    return test.area
+}
+
+test('hit function works', () => {
+    expect(testingHit(Ship(4), 2)).toMatchObject([0, 0, 1, 0]);
+});
+
+test('hit function works with a different number', () => {
+    expect(testingHit(Ship(4), 0)).toMatchObject([1, 0, 0, 0]);
 });
 
