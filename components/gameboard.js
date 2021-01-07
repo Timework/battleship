41d8bd8b8d251dad3fb7c337cf3fa31e2d899adc
Christopher Martinez <timework.bibo@gmail.com>
2021-01-07 04:40:25 -0600

    started working on the gameboard constructor

---
+++ components/gameboard.js
@@ -0,0 +1,80 @@
+const Gameboard = () => {
+
+    // keeps track of occupied area
+    let occupied = [];
+
+    // places a ship
+    const place = (ship, xCoordinate, yCoordinate, vertical) => {
+        // will hold the new ships coordinates until it is ready to be placed
+        let newShip = [];
+        // keeps track of the ship's size
+        let shipSize = ship.showSize();
+
+        // checks to make sure the coordinates are positive if not does not place the ship
+        if (!positiveChecker(xCoordinate, yCoordinate)) {
+            return;
+        };
+        
+        // places the ship as long as all the coordinates remains in bounds
+        if (vertical) {
+            if (boundChecker(shipSize, yCoordinate)) {
+                for (let i = 0; i < shipSize; i++){
+                    if (emptyChecker([xCoordinate, yCoordinate + i])){
+                        newShip.push([xCoordinate, yCoordinate + i]);
+                    } else {
+                        return;
+                    };
+                };
+            } else {
+                return;
+            };
+        } else {
+            if (boundChecker(shipSize, xCoordinate)) {
+                for (let i = 0; i < shipSize; i++){
+                    if (emptyChecker([xCoordinate + i, yCoordinate])){
+                        newShip.push([xCoordinate + i, yCoordinate]);
+                    } else {
+                        return;
+                    };
+                };
+            } else {
+                return;
+            };
+        };
+        // puts the ship in the object to keep track of hits
+        newShip.push(ship);
+        // the ship is placed on board
+        occupied.push(newShip);
+    };
+
+    // checks to see if the ship would be out of bounds
+    const boundChecker = (size, coordinate) => {
+        finalCoordinate = size + coordinate;
+        return finalCoordinate <= 11;
+    };
+
+    // checks to see if the ship's coordinates are positive
+    const positiveChecker = (xCoordinate, yCoordinate) => {
+        return (xCoordinate >= 0 && yCoordinate >= 0);
+    };
+
+    // checks to see if the coordinate is already occupied
+    const emptyChecker = (coordinates) => {
+        for (let i = 0; i < occupied.length; i++){
+           if (occupied[i].includes(coordinates)) {
+               return false;
+           };
+        };
+        return true;
+    };
+
+    // counts the ships
+    const ships = () => {
+        return occupied.length;
+    };
+
+    return {place, ships, occupied}
+
+};
+
+module.exports.Gameboard = Gameboard;
--- index.test.js
+++ index.test.js
@@ -1,103 +1,178 @@
 const rewire = require('rewire');
 
-// tests for the ship constructor
 const Ship = rewire('./components/ship').__get__("Ship");
+const Gameboard = rewire('./components/gameboard').__get__("Gameboard");
 
-test('ship length is correct', () => {
-    expect(Ship(4).showSize()).toBe(4);
-});
+describe('tests for the ship constructor', function () {
 
-test('checking ship length with a different number', () => {
-    expect(Ship(2).showSize()).toBe(2);
-});
+    test('ship length is correct', () => {
+        expect(Ship(4).showSize()).toBe(4);
+    });
 
-// this will check if you can change the size of a ship
-const sizeChanger = (ship, newSize) => {
-    ship.size = newSize;
-    return ship.showSize();
-};
+    test('checking ship length with a different number', () => {
+        expect(Ship(2).showSize()).toBe(2);
+    });
 
-test('changing the size wont work', () => {
-    expect(sizeChanger(Ship(2), 4)).toBe(2);
-});
-
-test('checking ship makes all the spots', () => {
-    expect(Ship(2).showArea()).toMatchObject([0, 0]);
-});
-
-test('checking ship makes all the spots with a different size', () => {
-    expect(Ship(3).showArea()).toMatchObject([0, 0, 0]);
-});
-
-// a function to help testing the hit function
-const testingHit = (test, number) => {
-    test.hit(number);
-    return test.showArea();
-}
-
-test('hit function works', () => {
-    expect(testingHit(Ship(4), 2)).toMatchObject([0, 0, 1, 0]);
-});
-
-test('hit function works with a different number', () => {
-    expect(testingHit(Ship(4), 0)).toMatchObject([1, 0, 0, 0]);
-});
-
-// a function to help test multiple hits
-const multipleHit = (test, first, second) => {
-    test.hit(first);
-    test.hit(second);
-    return test.showArea();
-}
-
-test('multiple hits work', () => {
-    expect(multipleHit(Ship(4), 2, 1)).toMatchObject([0, 1, 1, 0]);
-});
-
-test('multiple hits work with different numbers', () => {
-    expect(multipleHit(Ship(4), 0, 3)).toMatchObject([1, 0, 0, 1]);
-});
-
-test('multiple hits in the same area doesnt do anything', () => {
-    expect(multipleHit(Ship(4), 1, 1)).toMatchObject([0, 1, 0, 0]);
-});
-
-
-// this will make every position hit on a ship
-const sinkShip = (number) => {
-    const testShip = Ship(number);
-    for (let i = 0; i < number; i++){
-        testShip.hit(i);
+    // this will check if you can change the size of a ship
+    const sizeChanger = (ship, newSize) => {
+        ship.size = newSize;
+        return ship.showSize();
     };
-    return testShip;
-}
+
+    test('changing the size wont work', () => {
+        expect(sizeChanger(Ship(2), 4)).toBe(2);
+    });
+
+    test('checking ship makes all the spots', () => {
+        expect(Ship(2).showArea()).toMatchObject([0, 0]);
+    });
+
+    test('checking ship makes all the spots with a different size', () => {
+        expect(Ship(3).showArea()).toMatchObject([0, 0, 0]);
+    });
+
+    // a function to help testing the hit function
+    const testingHit = (test, number) => {
+        test.hit(number);
+        return test.showArea();
+    }
+
+    test('hit function works', () => {
+        expect(testingHit(Ship(4), 2)).toMatchObject([0, 0, 1, 0]);
+    });
+
+    test('hit function works with a different number', () => {
+        expect(testingHit(Ship(4), 0)).toMatchObject([1, 0, 0, 0]);
+    });
+
+    // a function to help test multiple hits
+    const multipleHit = (test, first, second) => {
+        test.hit(first);
+        test.hit(second);
+        return test.showArea();
+    }
+
+    test('multiple hits work', () => {
+        expect(multipleHit(Ship(4), 2, 1)).toMatchObject([0, 1, 1, 0]);
+    });
+
+    test('multiple hits work with different numbers', () => {
+        expect(multipleHit(Ship(4), 0, 3)).toMatchObject([1, 0, 0, 1]);
+    });
+
+    test('multiple hits in the same area doesnt do anything', () => {
+        expect(multipleHit(Ship(4), 1, 1)).toMatchObject([0, 1, 0, 0]);
+    });
 
 
-test('function to check if something is sunk', () => {
-    expect(sinkShip(4).isSunk()).toBe(true);
+    // this will make every position hit on a ship
+    const sinkShip = (number) => {
+        const testShip = Ship(number);
+        for (let i = 0; i < number; i++){
+            testShip.hit(i);
+        };
+        return testShip;
+    }
+
+
+    test('function to check if something is sunk', () => {
+        expect(sinkShip(4).isSunk()).toBe(true);
+    });
+
+    test('function to check if something is sunk with a different size ship', () => {
+        expect(sinkShip(2).isSunk()).toBe(true);
+    });
+
+    // almost sinks the ship
+    const almostSinkShip = (number) => {
+        const testShip = Ship(number);
+        for (let i = 0; i < number-1; i++){
+            testShip.hit(i);
+        };
+        return testShip;
+    }
+
+    test('will return false when the ship is not sunk', () => {
+        expect(almostSinkShip(2).isSunk()).toBe(false);
+    });
+
+    test('will return false when the ship is not sunk with a different size ship', () => {
+        expect(almostSinkShip(4).isSunk()).toBe(false);
+    });
+
+    test('will return false when the ship is not hit', () => {
+        expect(Ship(4).isSunk()).toBe(false);
+    });
+
 });
 
-test('function to check if something is sunk with a different size ship', () => {
-    expect(sinkShip(2).isSunk()).toBe(true);
-});
+describe('tests for the gameboard constructor', function () {
 
-// almost sinks the ship
-const almostSinkShip = (number) => {
-    const testShip = Ship(number);
-    for (let i = 0; i < number-1; i++){
-        testShip.hit(i);
+    // function to help place ships on the gameboard
+    const placeShip = (ship, xCoordinate, yCoordinate, vertical) => {
+        const testboard = Gameboard();
+        testboard.place(ship, xCoordinate, yCoordinate, vertical);
+        return testboard.ships();
     };
-    return testShip;
-}
 
-test('will return false when the ship is not sunk', () => {
-    expect(almostSinkShip(2).isSunk()).toBe(false);
-});
+    test('gameboard can properly place ships', () => {
+        expect(placeShip(Ship(2), 2, 3, true)).toBe(1);
+    });
 
-test('will return false when the ship is not sunk with a different number', () => {
-    expect(almostSinkShip(4).isSunk()).toBe(false);
-});
+    test('gameboard can properly place ships with different specs', () => {
+        expect(placeShip(Ship(4), 0, 0, false)).toBe(1);
+    });
 
-test('will return false when the ship is not hit', () => {
-    expect(Ship(4).isSunk()).toBe(false);
-});
+    // function to help place multiple ships on the gameboard
+    const placeMultipleShip = (ship, xCoordinate, yCoordinate, vertical) => {
+        const testboard = Gameboard();
+        testboard.place(ship, xCoordinate, yCoordinate, vertical);
+        testboard.place(ship, xCoordinate - 2, yCoordinate - 2, !vertical);
+        return testboard.ships();
+    };
+
+    test('gameboard can properly place more than 1 ship', () => {
+        expect(placeMultipleShip(Ship(4), 2, 2, true)).toBe(2);
+    });
+
+    test('gameboard can properly place more than 1 ship with different specs', () => {
+        expect(placeMultipleShip(Ship(2), 4, 4, false)).toBe(2);
+    });
+
+    test('gameboard will not place a ship that goes out of bounds', () => {
+        expect(placeShip(Ship(4), 8, 8, true)).toBe(0);
+    });
+
+    test('gameboard will not place a ship that goes out of bounds horiztonally', () => {
+        expect(placeShip(Ship(4), 8, 8, false)).toBe(0);
+    });
+
+    test('gameboard will still place a ship that is on the edge', () => {
+        expect(placeShip(Ship(1), 10, 10, false)).toBe(1);
+    });
+
+    test('gameboard will not place a ship on negative y coordinates', () => {
+        expect(placeShip(Ship(3), 0, -2, false)).toBe(0);
+    });
+
+    test('gameboard will not place a ship on negative x coordinates', () => {
+        expect(placeShip(Ship(3), -2, 0, false)).toBe(0);
+    });
+
+    test('gameboard will not place a ship on negative x & y coordinates', () => {
+        expect(placeShip(Ship(3), -2, -2, true)).toBe(0);
+    });
+
+    const sameShips = (ship, xCoordinate, yCoordinate, vertical) => {
+        const testboard = Gameboard();
+        testboard.place(ship, xCoordinate, yCoordinate, vertical);
+        testboard.place(ship, xCoordinate, yCoordinate, vertical);
+        // changed the occupired back to ships once you are done debugging
+        return testboard.occupied;
+    };
+
+    test('gameboard will not place a ship that overlaps with another ship', () => {
+        expect(sameShips(Ship(4), 0, 0, true)).toBe(1);
+    });
+
+  });
